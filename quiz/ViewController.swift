49215e64b0ec8c6f5d641f0d6270a3107292a332
Sommaya Haque <haques4@bxscience.edu>
2020-06-18 14:42:00 -0400

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Sommaya Haque on 6/18/20.
+//  Copyright © 2020 Sommaya Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
